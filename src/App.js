0b494fd1ac21865d9c161b38369c5bec0e1dab87
Ali Kim <alikim@gmail.com>
2022-08-09 19:04:01 -0400

    just the frontend, initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import { useState } from 'react';
+
 
 function App() {
+
+  // const [contractAddress, setContractAddress] = useState('');
+
+  const handleClick = () => {
+    // logic to handle click, using contract address
+    let inputVal = document.getElementById('inputbox').value;
+    console.log(inputVal);
+
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='title'>
+        <h1>Smart Contract Security Check</h1>
+
+      </div>
+
+
+      <div className='main'>
+
+        <input
+
+          id='inputbox'
+          type="text"
+          placeholder='address of contract to check'
+        />
+
+        <button className='button-7' onClick={handleClick}>
+          Run check
+        </button>
+      </div>
+
+      <div className='bgtag'>
+        Bg Photo by Scott Webb on Unsplash
+      </div>
+
     </div>
   );
 }
